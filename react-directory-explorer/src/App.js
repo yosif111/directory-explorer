b008874c126dbb4c18f68653a6dcca0202b85a1c
Yousef AlKhlefah <yosifmk96@gmail.com>
2019-12-30 21:21:06 +0300

    added material ui

--- react-directory-explorer/src/App.js
+++ react-directory-explorer/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
+import Container from '@material-ui/core/Container';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Container maxWidth="lg" className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </Container>
   );
 }
 
